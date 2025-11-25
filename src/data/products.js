ecd3018f210af89943cb6afcf4c90b0799687244
Dhruv00031 <dhruvbharadwaj001@gmail.com>
2025-11-25 16:19:11 +0530

    Add products data

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+﻿const products = [
+  {
+    id: 1,
+    name: 'Fiddle Leaf Fig',
+    price: 300,
+    category: 'Large Plants',
+    image: '/assets/fiddle-leaf.jpg'
+  },
+  {
+    id: 2,
+    name: 'Snake Plant',
+    price: 150,
+    category: 'Air Purifying',
+    image: '/assets/snake-plant.jpg'
+  },
+  {
+    id: 3,
+    name: 'Spider Plant',
+    price: 120,
+    category: 'Air Purifying',
+    image: '/assets/spider-plant.jpg'
+  },
+  {
+    id: 4,
+    name: 'Peace Lily',
+    price: 180,
+    category: 'Flowering',
+    image: '/assets/peace-lily.jpg'
+  },
+  {
+    id: 5,
+    name: 'Aloe Vera',
+    price: 100,
+    category: 'Medicinal',
+    image: '/assets/aloe-vera.jpg'
+  },
+  {
+    id: 6,
+    name: 'Areca Palm',
+    price: 350,
+    category: 'Large Plants',
+    image: '/assets/areca-palm.jpg'
+  }
+];
+
+export default products;
